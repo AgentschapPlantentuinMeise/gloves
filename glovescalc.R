cf956f33ae41ae442abc09917b544aef96564d15
Quentin Groom <quentin.groom@plantentuinmeise.be>
2021-04-17 19:50:26 +0200

    changed to use dates not years

--- glovescalc.R
+++ glovescalc.R
@@ -3,6 +3,9 @@ library(ggplot2)
 library(ggthemes)
 library(dplyr)
 library(reshape2)
+library(rinat)
+library(httr)
+
 
 ############################################################################################
 #The data were initially analyzed using the proportion of people wearing gloves in each year
@@ -16,9 +19,6 @@ handsglovesbatsAgg <- handsglovesbats2 %>% mutate(percent = glove/(glove+hand)) 
 
 # Get rid of years before 2008, because there is little data
 handsglovesbatsAgg <- handsglovesbatsAgg[handsglovesbatsAgg$year > 2007, ]    
-
-#handsglovesbats <- read_delim("handsglovesbats.tsv", "\t", escape_double = FALSE, col_types = cols(percentGloves = col_double(), year = col_integer()), trim_ws = TRUE)
-#View(handsglovesbats)
 
 attach(handsglovesbatsAgg)
 head(handsglovesbatsAgg)
@@ -80,6 +80,7 @@ ggsave(
 
 handsglovesbats <- read.csv("~/R/gloves/handsglovesbats.csv")
 
+
 handsglovesbats$gloveOrHand <- as.factor(handsglovesbats$gloveOrHand )
 handsglovesbats$gloveOrHand <- as.numeric(handsglovesbats$gloveOrHand)
 handsglovesbats$gloveOrHand <- handsglovesbats$gloveOrHand -2
@@ -88,21 +89,47 @@ handsglovesbats$gloveOrHand <- handsglovesbats$gloveOrHand *-1
 # Get rid of years before 2008, because there is little data
 handsglovesbats <- handsglovesbats[handsglovesbats$year > 2007, ]   
 
+
+
+for (row in 1:nrow(handsglovesbats)) {
+  
+  path <- parse_url(handsglovesbats[row,"observation"])$path
+  
+  iNaturalistID <- strsplit(path, "/")[[1]][2]
+  
+  handsglovesbats[row, "iNaturalistID"]   <- iNaturalistID
+  
+  #########################################################################
+  # BEWARE THIS CALLS THE iNATURALIST API AND SO SHOULD BE USE CAUTIOUSLY #
+  #########################################################################
+  observation <- get_inat_obs_id(iNaturalistID)
+  Sys.sleep(2)
+  handsglovesbats[row, "observed_on"] <- observation$observed_on
+}
+
+handsglovesbats$observed_on <- as.Date(handsglovesbats$observed_on)
+
+
 #handsglovesbats$gloveOrHand <- ordered(handsglovesbats$gloveOrHand, levels = c("hand", "glove"))
 
 #levels(handsglovesbats$gloveOrHand) <- c(0,1)
 
-modelLG <- glm(gloveOrHand ~year,family=binomial(link='logit'),data=handsglovesbats)
+modelLG <- glm(gloveOrHand ~ observed_on,family=binomial(link='logit'),data=handsglovesbats)
 
 summary(modelLG)
 
+# Odds Ratio
+oddrat <- exp(coef(modelLG))
+oddrat
+
 anova(modelLG, test="Chisq")
 
-ggplot(handsglovesbats, aes(x=year, y=gloveOrHand)) + 
-  geom_jitter(height = 0.03, width=0.3, shape=1, alpha = 0.4) +
-  scale_x_continuous(breaks = seq(2008, 2021, by = 3)) +
+ggplot(handsglovesbats, aes(x=observed_on, y=gloveOrHand)) + 
+  geom_point(height = 0.03, width=0.3, shape=16, alpha = 0.2, size=4) +
+  #geom_jitter(height = 0.03, width=0.3, shape=1, alpha = 0.4) +
+  #scale_x_continuous(breaks = seq(2008, 2021, by = 3)) +
   stat_smooth(formula = y ~ x, method="glm", method.args=list(family="binomial"), se=TRUE, fill = "steelblue", inherit.aes = TRUE, size = 1.5, linetype = 1, colour = "red") +
-  labs(y = "Probability of gloves being used", x = "Year") + theme_stata(base_size = 20, base_family = "sans", scheme = "s2color")
+  labs(y = "Probability of gloves being used", x = "Date observed") + theme_stata(base_size = 20, base_family = "sans", scheme = "s2color")
 
 ggsave(
   "gloveprobability.tiff",
